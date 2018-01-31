dd523e0d10ae0e8ad4b65b497c10c53f03d59fd0
clannadqs <cdozier3@students.kennesaw.edu>
2018-01-31 10:10:20 -0500

    Shipper.py issues fixed with parsing json configuration file

--- Shipper.py
+++ Shipper.py
@@ -1,13 +1,17 @@
 import json
-import pysnmp
 import os
-import
+import snmpInterface
+import sys
 
 
 class Shipper(object):
     def __init__(self, shipperType, configPath):
         self.shipperType = shipperType
         self.configPath = configPath
+
+
+        self.snmpInterface = snmpInterface.snmpInterface()
+        self.setOIDdict()
 
 
     def setConfigPath(self, path):
@@ -23,24 +27,28 @@ class Shipper(object):
     def getConfigPath(self):
         return self.configPath
 
-    @property
-    def getOIDdict(self):
-        OIDdict = {}
+
+    def setOIDdict(self):
+        self.OIDdict = {}
 
         with open(self.configPath) as config:
-            configDict = json.loads(config)
+            configDict = json.load(config)
 
-        for oidType in configDict[self.shipperType]["OIDs"]:
-            for oid in oidType:
+        sys.stdout.write('Initializing.')
+        for oidType in configDict['shipperTypes'][self.shipperType]['metrics']['OIDs']:
+
+            for oid in configDict['shipperTypes'][self.shipperType]['metrics']['OIDs'][oidType]:
+                sys.stdout.write('.')
                 if oidType == "Disk":
-                    OIDdict['Disk ' + oid['name'] + ' Path'] = oid['Path']
-                    OIDdict['Disk ' + oid['name'] + ' Available'] = self.
-                else
+                    self.OIDdict['Disk ' + oid['Name'] + ' Path'] = oid['Path']
+                    self.OIDdict['Disk ' + oid['Name'] + ' Available'] = self.snmpInterface.getSnmpResult(oid['Available'])
+                else:
+                    self.OIDdict[oidType + ' ' + oid] = self.snmpInterface.getSnmpResult(configDict["shipperTypes"][self.shipperType]['metrics']["OIDs"][oidType][oid])
 
 
-
-        return OIDdict
-
+    @property
+    def getOIDdict(self):
+        return self.OIDdict
 
 
 
@@ -60,3 +68,10 @@ class SoftwareMetricShipper(Shipper):
 
         #initialization of hardware type shipper
         Shipper.__init__("software", os.getcwd() + '/Config/shipperConfig.json')
+
+
+
+test = Shipper("hardware", os.getcwd() + '/Config/shipperConfig.json')
+
+test.getOIDdict
+print("\n\n" + test.getOIDdict)
