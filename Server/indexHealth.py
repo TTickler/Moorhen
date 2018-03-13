eca7435b84a6eaea33fa7743013dc65e5a0d1b06
clannadqs <cdozier3@students.kennesaw.edu>
2018-03-13 18:28:03 -0400

    wildcard working. Server indexHealth.py now handles standard thresholds without wildcard naming conventions

--- Server/indexHealth.py
+++ Server/indexHealth.py
@@ -18,40 +18,56 @@ class indexHealth(object):
 			self.expectedMetrics = json.load(expectedMetricsJson)
 	
 	#returns status (2,3,4) of previous #totalToQuery oidTypes   
-	def getCurrent(self ,greenTotal,oidType, hits_Source):
-		
+	def getCurrent(self ,greenTotal,oidType, hits_source):
+		print(hits_source)
 		#totalCount is a dictionary that keeps track of the count of OID statuses that are
 		# 2, 3, or 4. The fields are incremented each time a specific status is found. So if 
 		# CPU.1.Status = 2 totalCount would be {"2":1,"3":0,"4":0} after one iteration. This allows ease of
 		# importance comparisons (4 > 3, 3 > 2) 
-		totalCount = {"2":0, "3":0, "4":0}
+		totalCount = {"1": 0, "2":0, "3":0, "4":0}
 				
 		#loop to check status numbers 
-		for category in hits_Source:
-			if category == "nonNested" or category == "@timestamp" or category == "client":
+		for category in hits_source:
+			if category == "@timestamp" or category == "client":
 				continue
+
+                        elif category == "nonNested":
+				for metric in hits_source[category]:
+					
+					if str(metric) in self.expectedMetrics['expectedMetrics']['Threshold']:
+
+						#since certain metrics such as CPU Utilization can have a lower value being worse than a higher value
+						#we must allow an easily configurable way to handle this. getcompareType returns whether the flow of comparisons
+						#increase or decrease. CPU Util might look something like 90>80>70 where anything above 90 is healthy, 89-71 is
+						#a warning area and anything less than 70 is a critical state. getCompareType() will return 'increasing' for less than and 'decreasing' for greater than
+						compareType = self.getCompareType(self.expectedMetrics['expectedMetrics']['Threshold'][str(metric)])
+						
+						totalCount = self.thresholdChecker(category, metric, hits_source, compareType, totalCount, self.expectedMetrics['expectedMetrics']['Threshold'][str(metric)])
+						
+
+					else:
+						continue
+					
+
+
 			else:
 				#for hardware piece in passed in hits[_source]. This is all of the hits returned from an
 				#elasticsearch query in json formatting
-				for hwPiece in hits_Source[category]:	
-					for metric in hits_Source[category][hwPiece]:
+				for hwPiece in hits_source[category]:	
+					for metric in hits_source[category][hwPiece]:
 
 						#conditional for comparing the expectedMetrics to the current metric. If it is found
 						#then logic can be done to perform updates on totalCount's fields. Format of the current
 						#metric is converted to EX: CPU.1.Status to match schema of expectedMetrics and match
 						#how elasticsearch queries require their query to be formatted 
-						if (str(category + "." + hwPiece + "." + metric) in self.expectedMetrics['expectedMetrics']['Status']['healthy']) or (str(category + ".*." + metric) in self.expectedMetrics['expectedMetrics']['Status']['healthy']):
-							if hits_Source[category][hwPiece][metric] == self.expectedMetrics['expectedMetrics']['Status']['healthy'][str(category + "." + hwPiece + "." + metric)]:
-								totalCount["2"] += 1
-							elif hits_Source[category][hwPiece][metric] == self.expectedMetrics['expectedMetrics']['Status']['warning'][str(category + "." + hwPiece + "." + metric)]:
-								totalCount["3"] += 1
-							elif hits_Source[category][hwPiece][metric] == self.expectedMetrics['expectedMetrics']['Status']['failure'][str(category + "." + hwPiece + "." + metric)]:
-								totalCount["4"] += 1
-							else:
-								totalCount["1"] += 1
+						if str(category + "." + hwPiece + "." + metric) in self.expectedMetrics['expectedMetrics']['Status']['healthy']:
+							totalCount = self.statusMetricsChecker(category, hwPiece, metric, hits_source, totalCount)
 
+						elif str(category + ".*." + metric) in self.expectedMetrics['expectedMetrics']['Status']['healthy']:
+							totalCount = self.statusMetricsChecker(category, metric, hits_source, totalCount, hwPiece, wildcard=True)
 						else:
 							continue
+				
 		print(totalCount)
 
 		if totalCount["4"] != 0:
@@ -62,6 +78,59 @@ class indexHealth(object):
 			status = 2
 								
 		return status
+
+	def statusMetricsChecker(self, category, metric, hits_source, totalCount, hwPiece, wildcard=False):
+                            
+
+		#allows configuration to have wildcard for count of individual metric for brevity
+		#hwPieceHolder should probably be changed in name to be more relevant and readable  
+		if wildcard == True:
+			hwPieceHolder = '*'
+		else:
+			hwPieceHolder = hwPiece
+
+            	print(str(category + "." + hwPiece + "." + metric)) 
+		if hits_source[category][hwPiece][metric] == self.expectedMetrics['expectedMetrics']['Status']['healthy'][str(category + "." + hwPieceHolder + "." + metric)]:
+			totalCount["2"] += 1
+		elif hits_source[category][hwPiece][metric] == self.expectedMetrics['expectedMetrics']['Status']['warning'][str(category + "." + hwPieceHolder + "." + metric)]:
+			totalCount["3"] += 1
+		elif hits_source[category][hwPiece][metric] == self.expectedMetrics['expectedMetrics']['Status']['failure'][str(category + "." + hwPieceHolder + "." + metric)]:
+                        totalCount["4"] += 1
+                else:
+                        totalCount["1"] += 1
+
+		return totalCount
+
+	def getCompareType(self, metricDict):
+
+		if metricDict["healthy"] > metricDict["warning"] and metricDict["warning"] > metricDict["critical"]:
+			return 'decreasing'
+		else:
+			return 'increasing'
+
+
+	def thresholdChecker(self, category, metric ,hits_source, compareType, totalCount, metricDict):
+		print(metricDict)
+		print(hits_source[category][metric])	
+		if compareType == 'decreasing':
+			if hits_source[category][metric] > metricDict['healthy']:
+				totalCount["2"] += 1
+			elif (hits_source[category][metric] < metricDict['healthy']) and (hits_source[category][metric] > metricDict['critical']):
+				totalCount["3"] += 1
+
+			else:
+				totalCount["4"] += 1 
+
+		else:
+                        if hits_source[category][metric] > metricDict['critical']:
+                                totalCount["4"] += 1
+                        elif (hits_source[category][metric] > metricDict['healthy']) and (hits_source[category][metric] < metricDict['critical']):
+                                totalCount["3"] += 1
+
+                        else:
+                                totalCount["2"] += 1
+
+		return totalCount
 
 	def queryIndex(self, index, docType, elasticsearch, querySize):
 		esQuery = {"query": {
