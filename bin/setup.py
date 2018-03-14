c3a125bc1a3bd55970ce6031bac084028265ecf1
clannadqs <cdozier3@students.kennesaw.edu>
2018-03-14 16:30:30 -0400

    added distribution information through setup tools in bin/setup.py

--- bin/setup.py
+++ bin/setup.py
@@ -1,1 +1,14 @@
+#!/usr/bin/env python
+
 import pip
+from distutils.core import setup
+from setuptools import find_packages
+import os
+
+setup(
+    name='Shipper',
+    version='0.1dev',
+    packages=find_packages(),
+    license='',
+    long_description=open(os.getcwd() + '/../README.md').read(),
+)
