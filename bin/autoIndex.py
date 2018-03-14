dcb3ca3146f954ae556782d5f5794766dff67b81
clannadqs <cdozier3@students.kennesaw.edu>
2018-03-14 17:53:18 -0400

    alpha functionality of bin/autoIndex.py are working. Function for CLI arguments need to be added

--- bin/autoIndex.py
+++ bin/autoIndex.py
@@ -9,3 +9,23 @@ import json
 #mappings and expectedMetrics to a specified elasticsearch index for use by elasticsearch and 
 #queries to obtain expectedMetrics mappings from any node, or hub, on the same network. Allowing for
 #a one time automated indexing from each node at setup time.
+
+
+with open(os.getcwd() + '/../Config/generalConfig.json') as generalConfig:
+	gConfig = json.load(generalConfig)
+
+nodeName = gConfig['clientInfo']['name']
+esIndex = gConfig['mappingInfo']['esIndex']
+
+with open(os.getcwd() + '/../Config/expectedMetrics.json') as expectMetrics:
+	expectedMetrics = json.load(expectMetrics)
+
+try:
+	es = elasticsearch.Elasticsearch('http://localhost:9200')
+	response = es.index(index=esIndex,doc_type="expectedMetrics", body=expectedMetrics)
+
+except Exception as esException:
+	print(esException)
+
+
+
