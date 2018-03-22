63cd01a91aa3f23a9c1e44c3ee2e41c20c50e8dc
clannadqs <cdozier3@students.kennesaw.edu>
2018-03-22 12:36:01 -0400

    Added interface for Message to abstract and decouple messages from other modules/interfaces

---
+++ Shipper/message.py
@@ -0,0 +1,52 @@
+import json
+
+
+class Message(object):
+	def __init__(self):
+		self._endpoints = []
+		self._timestamp = ''
+		self._payload = {}
+		self._tags 
+
+	'''Acts similar to __init__(), but provides a more readable point of 
+		"entry" to this interface. This way it's abundantly clear that 
+		you are initializing a new message.'''
+	def initialize(self, timestamp='', endpoints=[], payload={}):
+		self.timestamp = timestamp
+		self.endpoints = endpoints
+		self.payload = json.dumps(payload) 
+
+	@property 
+	def timestamp(self):
+		return self._timestamp
+
+	@timestamp.setter
+	def timestamp(self, time):
+		self._timestamp = time
+
+	@property
+	def endpoints(self):
+		return self._endpoints
+
+	@endpoints.setter
+	def endpoints(self, endpoints):
+		self._endpoints = endpoints
+
+	@property
+	def payload(self):
+		return self._payload
+
+	@payload.setter
+	def payload(self, payload):
+		self._payload = payload 
+
+	@property
+	def tags(self):
+		return self._tags
+
+	@tags.setter
+	def tags(self, tags):
+		self._tags = tags
+
+
+
