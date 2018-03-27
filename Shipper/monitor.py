61f8981f331e838039c6fac0e233d47aa7830e1e
clannadqs <cdozier3@students.kennesaw.edu>
2018-03-27 12:09:53 -0400

    Added subprocess function in parent interface of monitor module. This allows children to make system calls.

--- Shipper/monitor.py
+++ Shipper/monitor.py
@@ -1,6 +1,8 @@
 import c_parser
 import os
-import sys
+
+#subprocess module is used here in place of sys to avoid overhead and be more pythonic 
+import subprocess
 
 '''Parent class of specialized monitors for directories, processes, and metrics'''
 class Monitor(object):
@@ -11,16 +13,34 @@ class Monitor(object):
     def monitor_type(self):
         return self._monitor_type
 
-    @type.setter
+    @monitor_type.setter
     def monitor_type(self, monitor_type):
-        self._monitor_type = monitor_type  
+        self._monitor_type = monitor_type
+
+    def sys_command(self, command):
+        
+	try:
+            command_result = subprocess.check_output([command])
+
+	except:
+	    print("Invalid System Command...")
+	return command_result  
 
 '''Child class of Monitor. Focuses on handling monitoring a directory'''
 class DirectoryMonitor(Monitor):
     def __init__(self):
         Monitor.__init__(self)
 
-    def 
+        self._mappings = { 
+
+
+
+
+                        }
+
+
+
+    #def get_largest_file(self): 
 
 
 '''Child class of Monitor. Focuses on handling monitoring metrics'''
@@ -28,11 +48,37 @@ class MetricMonitor(Monitor):
     def __init__(self):
         Monitor.__init__(self)
 
+        self._mappings = { 
+
+
+
+
+                        }
+
+
+
+
+
+
 '''Child class of Monitor. Focuses on handling monitoring a process'''
 class ProcessMonitor(Monitor):
     def __init__(self, process_message):
         Monitor.__init__(self)
 	
+	self._mappings = { 
 
-    def get_mem_used(self):
-    def get_cpu_used(self):
+
+
+
+			}
+
+    #def get_mem_used(self):
+    #def get_cpu_used(self):
+
+
+
+test = Monitor()
+
+print(test.sys_command("ls"))
+raw_in = raw_input()
+print(globals())
